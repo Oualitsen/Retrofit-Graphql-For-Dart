17980086797d40ef230a54fa26f7f133e6b8b9a3
ramdane.oualitsen@externe.sacem.fr <ramdane.oualitsen@externe.sacem.fr>
2025-07-30 16:18:15 +0200

    common-field-extraction-on-projected-interfaces

---
+++ test/interface_common_fields/interface_common_fields_test.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:retrofit_graphql/src/serializers/dart_client_serializer.dart';
+import 'package:retrofit_graphql/src/serializers/dart_serializer.dart';
+import 'package:test/test.dart';
+import 'package:retrofit_graphql/src/gq_grammar.dart';
+import 'package:petitparser/petitparser.dart';
+
+final GQGrammar g = GQGrammar();
+
+void main() async {
+  test("common interface fields", () {
+    final text = File(
+            "test/interface_common_fields/interface_common_fields_test.graphql")
+        .readAsStringSync();
+    final GQGrammar g = GQGrammar(
+        generateAllFieldsFragments: false, autoGenerateQueries: false);
+
+    var parsed = g.parse(text);
+    expect(parsed is Success, true);
+    var basicEntityInterface = g.projectedTypes["BasicEntity"]!;
+    expect(basicEntityInterface.fieldNames.length, 2);
+    expect(basicEntityInterface.fieldNames, containsAll(["id", "lastUpdate"]));
+  });
+}
