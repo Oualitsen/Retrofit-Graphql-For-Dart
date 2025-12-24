066bbfa410392b5655158c64939e5d3a0138ae31
Ramdane <oualitsenramdane@gmail.com>
2025-12-24 12:54:28 +0100

    fix-failing-test-on-directives

--- test/directives_test.dart
+++ test/directives_test.dart
@@ -2,10 +2,9 @@ import 'package:test/test.dart';
 import 'package:retrofit_graphql/src/gq_grammar.dart';
 import 'package:petitparser/petitparser.dart';
 
-final GQGrammar g = GQGrammar();
-
 void main() {
   test("Directive value  test", () {
+    final g = GQGrammar();
     var parser = g.buildFrom(g.directiveValue().end());
     var result = parser.parse('''
       @skip(if: true)
@@ -22,6 +21,7 @@ void main() {
   });
 
   test("Directive scopes test", () {
+    final g = GQGrammar();
     var parser = g.buildFrom(g.directiveScopes().end());
     var result = parser.parse('''
     SCALAR|OBJECT|INTERFACE | ARGUMENT_DEFINITION
@@ -30,6 +30,7 @@ void main() {
   });
 
   test("Directive definition test", () {
+    final g = GQGrammar();
     var parser = g.buildFrom(g.directiveDefinition().end());
     var result = parser.parse('''
     directive @test(test: String!, arg2: [String!]! ) on INTERFACE | ARGUMENT_DEFINITION
@@ -38,6 +39,7 @@ void main() {
   });
 
   test("Directive definition test2", () {
+    final g = GQGrammar();
     var parser = g.buildFrom(g.directiveDefinition().end());
     var result = parser.parse('''
     directive @gqDecorator(value: String) on  OBJECT | INPUT_OBJECT| FIELD_DEFINITION | FIELD
@@ -46,6 +48,7 @@ void main() {
   });
 
   test("Directive definition repeatable test", () {
+    final g = GQGrammar();
     var parser = g.buildFrom(g.directiveDefinition().end());
     var result = parser.parse('''
     directive @gqDecorator(value: String) repeatable on  OBJECT | INPUT_OBJECT| FIELD_DEFINITION | FIELD
